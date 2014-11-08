ac4507bc1ee4ae3db8f6502d688e8dac8306a43d
gigsda <gigsda@gmail.com>
2014-11-09 06:04:48 +0900

    change passwd feature, but notworking ssh-copy-id

--- create-containers.sh
+++ create-containers.sh
@@ -1,6 +1,26 @@
+export PASSWD=1234
+
 for host in `cat hosts.lst` ;
 do
   echo $host 
   lxc-create -t ubuntu -n $host
   sleep 2
+done
+
+sh start-containers.sh
+
+for host in `cat hosts.lst` ;
+do
+  lxc-attach -n $host -- /bin/sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+  lxc-attach -n $host -- /bin/sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+  lxc-attach -n $host -- service ssh reload
+  echo root:$PASSWD | lxc-attach -n $host -- chpasswd
+
+#  expect -c "
+#     spawn ssh-copy-id root@$host
+#     expect \"*re you sure you want to continue connecting\" 
+#     send \"yes\\r\"
+#     expect \"*assword*\" 
+#     send \"$PASSWD\\r\"
+#  "
 done
